933016addc46f74cb680eb90f5844c2182b4db16
AleeAsto <astocondor.alejandra@gmail.com>
2025-09-24 19:46:19 -0500

    GomiAnimsV1

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -6,11 +7,18 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     public float jumpForce = 17f;
     private Rigidbody2D rb;
+
+    private Animator anim;
     private bool isGrounded;
+
+    [Header("Referencias")]
+    public SpriteRenderer sr;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -18,10 +26,53 @@ public class PlayerMovement : MonoBehaviour
         float move = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
 
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            StartCoroutine(JumpWithDelay());
+            anim.SetBool("Jumping", true);
         }
+
+        //CAIDA?
+        if (rb.linearVelocity.y < -0.1f && !isGrounded)
+        {
+            anim.SetBool("Falling", true);
+             
+        }
+        else
+        {
+            anim.SetBool("Falling", false);
+        }
+
+        //AGACHARSE: NOTA, LUEGO QUITAR EL HARDCODE DE LA TECLA X'd
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.SetBool("Down", true);
+        }
+        else
+        {
+            anim.SetBool("Down", false);
+        }
+
+        if (move > 0)
+            sr.flipX = false; // mira a la derecha
+        else if (move < 0)
+            sr.flipX = true;  // mira a la izquierda
+
+
+    }
+
+    private IEnumerator JumpWithDelay()
+    {
+        // Opcional: activar animación de "pre-salto"
+        anim.SetBool("Jumping", true);
+
+        // Esperar 1/12 de segundo (~0.083s)
+        yield return new WaitForSeconds(2f / 24f);
+
+        // Ejecutar el salto
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -29,6 +80,8 @@ public class PlayerMovement : MonoBehaviour
         if (collision.contacts[0].normal.y > 0.5f)
         {
             isGrounded = true;
+            anim.SetBool("Jumping", false);
+            anim.SetBool("Falling", false);
         }
     }
 
